bf30dc98ba43c57d3d5c4f0d7b3a6ddffe651a75
Ankit Bali <ankit.bali@nagarro.com>
2018-09-21 15:42:17 +0530

    formatting

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,28 +3,23 @@ namespace WishList.Controller
     using System.Linq;
     using Microsoft.AspNetCore.Mvc;
     using WishList.Data;
-
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public ItemController(ApplicationDbContext context)
         {
             _context = context;
         }
-
         public IActionResult Index()
         {
             var model = _context.Items.ToList();
             return View("Index", model);
         }
-
         [HttpGet]
         public IActionResult Create()
         {
             return View("Create");
         }
-
         [HttpPost]
         public IActionResult Create(WishList.Models.Item item)
         {
@@ -32,7 +27,6 @@ namespace WishList.Controller
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-
         public IActionResult Delete(int id)
         {
             var itemToBeDeleted = _context.Items.Where(x => x.Id == id).FirstOrDefault();
