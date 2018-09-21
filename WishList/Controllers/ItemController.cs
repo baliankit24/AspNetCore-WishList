4e34fb44583ad1b6ae2ca7abf632bcdbf117f7cf
Ankit Bali <ankit.bali@nagarro.com>
2018-09-21 16:00:30 +0530

    typo for test case pass

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -29,7 +29,7 @@ namespace WishList.Controller
         }
         public IActionResult Delete(int id)
         {
-            var item = _context.Items.Where(e => e.Id == id).FirstOrDefault();
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
