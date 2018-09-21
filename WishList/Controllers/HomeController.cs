2ade71f7eabddb68db14d56cd2de01e96137b849
Ankit Bali <ankit.bali@nagarro.com>
2018-09-21 12:54:44 +0530

    added home controller and views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Error()
+    {
+        return View();
+    }
+}
